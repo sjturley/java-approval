5d13b32ad9dcaaf7c0d2446825c1b971b35ca26b
Lars Eckart <lars.eckart@gmail.com>
2020-04-26 12:42:15 +0300

    <wip> migrate to java14, junit5 and approval tests 4.0.2

--- start_point/HikerTest.java
+++ start_point/HikerTest.java
@@ -1,15 +1,14 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.*;
 
 import org.approvaltests.Approvals;
 import org.approvaltests.reporters.UseReporter;
 import org.approvaltests.reporters.JunitReporter;
 
 @UseReporter(JunitReporter.class)
-public class HikerTest {
+class HikerTest {
 
     @Test
-    public void hhgttg() throws Exception {
+    void hhgttg() throws Exception {
         int actual = Hiker.answer();
         Approvals.verify("" + actual);
     }
--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,6 +1,16 @@
-rm -f *.class
+#! /bin/bash
+set -e
+
+cd ${CYBER_DOJO_SANDBOX}
+
 CLASSES=.:`ls /approval/*.jar | tr '\n' ':'`
-javac -Xlint:unchecked -cp $CLASSES *.java
-if [ $? -eq 0 ]; then
-  java -cp $CLASSES org.junit.runner.JUnitCore `ls -1 *Test*.class | sed 's/\(.*\)\..*/\1/'`
+
+if javac --enable-preview --release 14 -Xlint:preview -Xlint:unchecked -Xlint:deprecation -cp $CLASSES *.java; then
+  java --enable-preview -jar /approval/junit-platform-console-standalone-1.6.2.jar \
+      --disable-banner \
+      --disable-ansi-colors \
+      --details=tree \
+      --details-theme=ascii \
+      --class-path .:$CLASSES \
+      --scan-class-path
 fi
